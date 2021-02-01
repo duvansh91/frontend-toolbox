0c237f0033610509122493d412f22fcbd9838adb
duvansh91 <jaimeduvan1996@gmail.com>
2021-01-31 21:23:06 -0500

    feat: Add entire project

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const htmlWebpack = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['@babel/polyfill','./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpack({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    port: 8000
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
